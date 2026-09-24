ac7d356918255788cfca2e77e1acceec75a43456
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-24 23:38:52 +0400

    feat(wgshim): install WGShim command

--- install.sh
+++ install.sh
@@ -234,6 +234,7 @@ for spec in \
   "bpc-subscription-url:bpc-subscription-url.sh" \
   "bpc-enable-awg:bpc-enable-awg.sh" \
   "bpc-enable-wg:bpc-enable-wg.sh" \
+  "bpc-enable-wgshim:bpc-enable-wgshim.sh" \
   "bpc-enable-mihomo-transports:bpc-enable-mihomo-transports.sh" \
   "bpc-enable-openvpn:bpc-enable-openvpn.sh" \
   "bpc-enable-ikev2:bpc-enable-ikev2.sh" \
@@ -310,6 +311,7 @@ Commands:
   bpc-subscription-url
   bpc-enable-awg
   bpc-enable-wg
+  bpc-enable-wgshim
   bpc-enable-mihomo-transports
   bpc-enable-openvpn
   bpc-enable-ikev2
