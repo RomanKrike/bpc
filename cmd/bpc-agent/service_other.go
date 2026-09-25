07392f5cd18d12a9a0f99629e2cc3732fc94dcc0
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-25 18:40:41 +0400

    feat: add service startup mode stub

--- cmd/bpc-agent/service_other.go
+++ cmd/bpc-agent/service_other.go
@@ -14,6 +14,10 @@ func installWindowsService(string) error {
 	return errors.New("Windows service support is unavailable on this platform")
 }
 
+func setWindowsServiceAutomatic(bool) error {
+	return errors.New("Windows service support is unavailable on this platform")
+}
+
 func startWindowsService() error {
 	return errors.New("Windows service support is unavailable on this platform")
 }
