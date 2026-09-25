a158534f4b9ca5586d4c3d8d08b8916e136b3cbe
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-25 10:16:32 +0400

    chore(agent): remove obsolete scheduled-task name

--- cmd/bpc-agent/main.go
+++ cmd/bpc-agent/main.go
@@ -23,7 +23,6 @@ import (
 
 const (
 	version         = "0.10.0"
-	taskName        = "BPC Agent"
 	bootstrapStart  = "\nBPC_AGENT_BOOTSTRAP_V2\n"
 	bootstrapEnd    = "\nBPC_AGENT_BOOTSTRAP_END\n"
 	defaultLogEvery = 30 * time.Second
