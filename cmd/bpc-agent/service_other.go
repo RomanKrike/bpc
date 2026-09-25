6ea1f821e6084af20f1d07643c54d01804ee6089
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-25 10:15:38 +0400

    test(agent): add non-Windows service stubs

---
+++ cmd/bpc-agent/service_other.go
@@ -0,0 +1,31 @@
+//go:build !windows
+
+package main
+
+import "errors"
+
+const serviceName = "BPCAgent"
+
+func runWindowsService() error {
+	return errors.New("Windows service support is unavailable on this platform")
+}
+
+func installWindowsService(string) error {
+	return errors.New("Windows service support is unavailable on this platform")
+}
+
+func startWindowsService() error {
+	return errors.New("Windows service support is unavailable on this platform")
+}
+
+func stopWindowsService() error {
+	return nil
+}
+
+func removeWindowsService() error {
+	return nil
+}
+
+func windowsServiceStatus() string {
+	return "unsupported"
+}
