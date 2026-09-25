ac5bf477a853be63abcadb236518a6302066090a
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-25 23:21:35 +0400

    chore(release): set agent version 0.11.2

--- cmd/bpc-agent/main.go
+++ cmd/bpc-agent/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	version         = "0.11.1"
+	version         = "0.11.2"
 	legacyTaskName  = "BPC Agent"
 	bootstrapStart  = "\nBPC_AGENT_BOOTSTRAP_V2\n"
 	bootstrapEnd    = "\nBPC_AGENT_BOOTSTRAP_END\n"
