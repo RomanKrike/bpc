7636d8fd9f0def6dbb83a7849269a43453b7a24a
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-25 18:37:04 +0400

    style: gofmt Agent UI stubs

--- cmd/bpc-agent/ui_other.go
+++ cmd/bpc-agent/ui_other.go
@@ -5,6 +5,6 @@ package main
 import "errors"
 
 func installWindowsUI(string, string) error { return errors.New("BPC Agent UI requires Windows") }
-func startWindowsUI() error { return errors.New("BPC Agent UI requires Windows") }
-func removeWindowsUI() error { return nil }
-func launchWindowsUI() error { return errors.New("BPC Agent UI requires Windows") }
+func startWindowsUI() error                 { return errors.New("BPC Agent UI requires Windows") }
+func removeWindowsUI() error                { return nil }
+func launchWindowsUI() error                { return errors.New("BPC Agent UI requires Windows") }
